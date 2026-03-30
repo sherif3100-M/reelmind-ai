4664cd72ab4eed1625cd8ead7ec286ed56937723
sherif3100 <mohamedsherif3100@gmail.com>
2026-03-30 15:30:14 +0530

    fix: inline app.html content

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
-import { useEffect } from 'react'
+import fs from 'fs'
+import path from 'path'
 
-export default function Home() {
-  useEffect(() => {
-    window.location.replace('/app.html')
-  }, [])
-  return null
+export default function Home({ html }) {
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
+}
+
+export async function getServerSideProps() {
+  const filePath = path.join(process.cwd(), 'public', 'app.html')
+  const html = fs.readFileSync(filePath, 'utf8')
+  // Extract body content
+  const bodyMatch = html.match(/<body[^>]*>([\s\S]*)<\/body>/i)
+  return {
+    props: { html: bodyMatch ? bodyMatch[1] : html }
+  }
 }
