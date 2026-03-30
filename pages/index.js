3a4a2bb8e866e1661c5c4d47d2c44c0489d9e3d5
sherif3100 <mohamedsherif3100@gmail.com>
2026-03-30 15:37:48 +0530

    fix: disable cache on redirect

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 export async function getServerSideProps({ res }) {
+  res.setHeader('Cache-Control', 'no-store')
   res.writeHead(302, { Location: '/app.html' })
   res.end()
   return { props: {} }
